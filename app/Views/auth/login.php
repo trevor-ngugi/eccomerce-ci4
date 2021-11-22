9f419bc763d513e74b8f0c25aaecc77fc4c5e7b0
trevor ngugi <ngugitrevor@gmail.com>
2021-11-22 23:57:13 +0300

    add register and login html forms

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -7,6 +7,26 @@
     </head>
     <body>
         <h1> this is the login page<h1>
+        <form action="/action_page.php">
+  <div class="container">
+    <h1>login</h1>
+    <hr>
+
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" id="email" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
+
+    
+    <hr>
+    <button type="submit" class="registerbtn">Register</button>
+  </div>
+  
+  <div class="container signin">
+    <p>do not have an account? <a href="#">register</a></p>
+  </div>
+</form>
 
         <a href="<?php echo site_url('auth/processLogin');?>">Process Login</a>
     </body>
