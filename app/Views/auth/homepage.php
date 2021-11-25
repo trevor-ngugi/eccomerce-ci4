25c28fd10132eb719826dcf1d7bd32980269e8db
trevor ngugi <ngugitrevor@gmail.com>
2021-11-25 15:24:38 +0300

    edited login page and added  css ,images

--- app/Views/auth/homepage.php
+++ app/Views/auth/homepage.php
@@ -1,36 +1,37 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>home page</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width-device-width ,inital-scale=1">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link href="<?php echo base_url('css/styles.css')?>" rel="stylesheet">
-        <script type="text/javascript"  src="<?php echo base_url('js/jquery.min.js')?>"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    </head>
-    <body>
-        <h1>users home page<h1>
-        <?php
-        $session=session();
-        $user_details=$session->get('user_details');
-       // echo "<pre>";print_r($user_details);die;
-       $firstname=$user_details[0];
-       $lastname=$user_details[1];
-       $fullname=$firstname." ".$lastname;
-       echo "welcome $fullname";
-        
-        ?>
-        <br>
-        <button id="images_toggle_btn" class="btn btn-primary">toggle image show</button>
-        <br>
-        <div id="images_div">
-        <img src="<?php echo base_url('images/pic1.jpeg');?>" alt="image from splash">
-        <br>
-        <img src="<?php echo base_url('images/pic2.jpeg');?>" alt="boy">
-        </div>
-        <script type="text/javascript" src="<?php echo base_url('js/main.js')?>"></script>
-    </body>
+
+<head>
+    <title>home page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width-device-width ,inital-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="<?php echo base_url('css/styles.css') ?>" rel="stylesheet">
+    <script type="text/javascript" src="<?php echo base_url('js/jquery.min.js') ?>"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <h1>users home page<h1>
+            <?php
+            $session = session();
+            $user_details = $session->get('user_details');
+            // echo "<pre>";print_r($user_details);die;
+            $firstname = $user_details[0];
+            $lastname = $user_details[1];
+            $fullname = $firstname . " " . $lastname;
+            echo "welcome $fullname";
+
+            ?>
+            <br>
+            <button id="images_toggle_btn" class="btn btn-primary">toggle image show</button>
+            <br>
+            <div id="images_div">
+                <img src="<?php echo base_url('images/pic1.jpeg'); ?>" alt="image from splash">
+                <br>
+                <img src="<?php echo base_url('images/pic2.jpeg'); ?>" alt="boy">
+            </div>
+            <script type="text/javascript" src="<?php echo base_url('js/main.js') ?>"></script>
+</body>
+
 </html>
--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -1,33 +1,38 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>login</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width-device-width ,inital-scale=1">
-    </head>
-    <body>
-        <h1> this is the login page<h1>
-        <form action="/action_page.php">
-  <div class="container">
-    <h1>login</h1>
-    <hr>
 
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" id="email" required>
+<head>
+  <title>login</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width-device-width ,inital-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="<?php echo base_url('css/login.css') ?>" rel="stylesheet">
+    <script type="text/javascript" src="<?php echo base_url('js/jquery.min.js') ?>"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://use.fontawesome.com/22de2a47bc.js"></script>
+</head>
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
+<body>
+      <form action="/action_page.php">
+        <div class="container">
+          <h1>Member login</h1>
+          <hr>
+          <i class="fa fa-envelope" aria-hidden="true"></i>
+          <input type="text" placeholder="Enter Email" name="email" id="email" required><br><br>
 
-    
-    <hr>
-    <button type="submit" class="registerbtn">Register</button>
-  </div>
-  
-  <div class="container signin">
-    <p>do not have an account? <a href="#">register</a></p>
-  </div>
-</form>
+          <i class="fa fa-lock" aria-hidden="true"></i>
+          <input type="password" placeholder="Enter Password" name="psw" id="psw" required><br>
+          <hr>
+          <button type="submit" class="btn btn-info">Login</button>
+        </div>
 
-        <a href="<?php echo site_url('auth/processLogin');?>">Process Login</a>
-    </body>
+        <div class="container signin">
+          <p>do not have an account? <a href="#">register</a></p>
+        </div>
+      </form>
+
+      <a href="<?php echo site_url('auth/processLogin'); ?>">Process Login</a>
+      <script type="text/javascript" src="<?php echo base_url('js/main.js') ?>"></script>
+</body>
+
 </html>
